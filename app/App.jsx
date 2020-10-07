d734b162c0c5906a94e8db1be72f3ab029499436
doughertyg <doughertyg37@gmail.com>
2020-10-07 00:48:51 -0700

    make functional, remove class, add hooks

--- app/App.jsx
+++ app/App.jsx
@@ -1,46 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { fetchJobsURI } from '../server/endpoints';
 import Notification from './components/notification/notification.jsx';
 import Button from './components/button/button.jsx';
 import JobsTable from './components/jobsTable/jobsTable.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App(props) {
+  const [jobs, setJobs] = useState([]);
+  const [error, setError] = useState('');
 
-    this.state = {
-      errorState: '',
-      jobs: []
-    }
-  }
-
-  componentDidMount() {
+  function fetchJobsAndSetState() {
     axios.get(fetchJobsURI)
       .then(({ data }) => {
-        this.setState({
-          jobs: data
-        })
+        setJobs(data);
       })
       .catch(err => {
         // set error on state
         // TODO: error state and message should be separate and explicit
-        this.setState({
-          errorState: err
-        })
+        setError(err);
       })
   }
 
-  render () {
+  useEffect(() => {
+    fetchJobsAndSetState();
+  }, [])
 
-    return (
-      <div className="app-wrapper">
-        {this.state.errorState && <Notification msg={this.state.errorState} />}
-        <Button>"Refresh"</Button>
-        <JobsTable data={this.state.jobs} />
-      </div>
-    )
+  function updateJobData() {
+    // axios.put...
   }
-}
+
+  return (
+    <div className="app-wrapper">
+      {error && <Notification msg={error} />}
+      <Button>"Refresh"</Button>
+      <JobsTable data={jobs} />
+    </div>
+  )
+};
 
 export default App;
